55a9346d5dcf3a2167600e80dbf8e93b033a25a7
anjakefala <anja@voltrondata.com>
2022-05-31 19:00:14 -0700

    move plugins-sample within cookiecutter scope and remove space around email

--- {{cookiecutter.plugin_name}}/{{cookiecutter.plugin_name}}.py
+++ {{cookiecutter.plugin_name}}/{{cookiecutter.plugin_name}}.py
@@ -1,6 +1,6 @@
 '''{{cookiecutter.description}}'''
 
-__author__ = '{{cookiecutter.author_name}} < {{cookiecutter.github_email}} >'
+__author__ = '{{cookiecutter.author_name}} <{{cookiecutter.github_email}}>'
 __version__ = '{{cookiecutter.initial_version}}'
 
 vd.option('disp_hello', 'Hello world!', 'string to display for hello-world command')
